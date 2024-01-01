b1b3a3ef055ab3534e4a75852fef1f7831a125a2
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-01 23:41:32 +0100

    Adding addTask to useTasks hook

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,20 +1,28 @@
 import uuid from "react-native-uuid";
-import { atom, useAtom } from "jotai";
+import { useAtom } from "jotai";
+import { atomWithImmer } from "jotai-immer";
 
 const uuid1 = uuid.v4() as string;
 const uuid2 = uuid.v4() as string;
 const uuid3 = uuid.v4() as string;
 
-const tasksAtom = atom({
+const tasksAtom = atomWithImmer({
   [uuid1]: { name: "Read a book", isCompleted: false },
   [uuid2]: { name: "Buy groceries", isCompleted: false },
   [uuid3]: { name: "Do laundry", isCompleted: true },
 });
 
 const useTasks = () => {
-  const [tasks] = useAtom(tasksAtom);
+  const [tasks, setTasks] = useAtom(tasksAtom);
 
-  return { tasks };
+  const addTask = () => {
+    setTasks((draft) => {
+      const taskUuid = uuid.v4() as string;
+      draft[taskUuid] = { name: "test", isCompleted: false };
+    });
+  };
+
+  return { tasks, addTask };
 };
 
 export default useTasks;
