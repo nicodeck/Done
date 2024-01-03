1d18b2a4722093629a09c42a3deac4bd203fe32a
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-03 23:06:55 +0100

    Refactor useTasks.ts to use jotai's atom and immer's produce

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,12 +1,12 @@
 import uuid from "react-native-uuid";
-import { useAtom } from "jotai";
-import { atomWithImmer } from "jotai-immer";
+import { atom, useAtom } from "jotai";
+import { produce } from "immer";
 
 const uuid1 = uuid.v4() as string;
 const uuid2 = uuid.v4() as string;
 const uuid3 = uuid.v4() as string;
 
-const tasksAtom = atomWithImmer({
+const tasksAtom = atom({
   [uuid1]: { name: "Read a book", isCompleted: false },
   [uuid2]: { name: "Buy groceries", isCompleted: false },
   [uuid3]: { name: "Do laundry", isCompleted: true },
@@ -16,16 +16,20 @@ const useTasks = () => {
   const [tasks, setTasks] = useAtom(tasksAtom);
 
   const addTask = () => {
-    setTasks((draft) => {
-      const taskUuid = uuid.v4() as string;
-      draft[taskUuid] = { name: "test", isCompleted: false };
-    });
+    setTasks(
+      produce((draft) => {
+        const taskUuid = uuid.v4() as string;
+        draft[taskUuid] = { name: "test", isCompleted: false };
+      })
+    );
   };
 
   const toggleTaskIsCompleted = (taskKey: string) => {
-    setTasks((draft) => {
-      draft[taskKey].isCompleted = !draft[taskKey].isCompleted;
-    });
+    setTasks(
+      produce((draft) => {
+        draft[taskKey].isCompleted = !draft[taskKey].isCompleted;
+      })
+    );
   };
 
   return { tasks, addTask, toggleTaskIsCompleted };
