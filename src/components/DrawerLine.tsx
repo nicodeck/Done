bf9cc32228e339878e0b412fde63902a0ca1b7a6
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-05 19:28:49 +0100

    Add DrawerLine component and fill TodoDrawer

---
+++ src/components/DrawerLine.tsx
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function DrawerLine() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>DrawerLine</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#00000022",
+    marginRight: 16,
+    marginVertical: 8,
+    paddingVertical: 16,
+    paddingLeft: 16,
+    paddingRight: 8,
+    borderTopRightRadius: 50,
+    borderBottomRightRadius: 50,
+    borderColor: "grey",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
--- src/components/TodoDrawer.tsx
+++ src/components/TodoDrawer.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import DrawerLine from "./DrawerLine";
 
 export default function TodoDrawer() {
   return (
     <View>
-      <Text>This is the Drawer</Text>
+      <View style={styles.header}>
+        <Pressable style={styles.closeButton}>
+          <Ionicons name="close-outline" size={32} color="black" />
+        </Pressable>
+        <Text style={styles.headerText}>My Todo Lists</Text>
+      </View>
+      <DrawerLine />
+      <DrawerLine />
+      <DrawerLine />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    paddingRight: 8,
+    paddingVertical: 24,
+    flexDirection: "row",
+  },
+  closeButton: {
+    paddingHorizontal: 16,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
