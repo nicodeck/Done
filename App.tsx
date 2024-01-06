ea54fad705ee4ce839e5a36ebdfcb3de075f1f8e
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-06 12:20:17 +0100

    Add StrictMode to App component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
+import { StrictMode } from "react";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <HomeScreen />
-    </SafeAreaProvider>
+    <StrictMode>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
+    </StrictMode>
   );
 }
