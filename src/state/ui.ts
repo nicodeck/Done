20bffe80d8046bc473ee30d03aa14ab33e8f8a5e
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-13 13:16:53 +0100

    Add complex behavior for current todo list key and tests

--- src/state/ui.ts
+++ src/state/ui.ts
@@ -1,3 +1,23 @@
 import { atom } from "jotai";
+import { TodoListsAtom } from ".";
 
-export const currentTodoListKeyAtom = atom<string>("");
+const currentTodoListKeyPrimitiveAtom = atom("");
+
+export const currentTodoListKeyAtom = atom(
+  (get) => {
+    const currentTodoListKey = get(currentTodoListKeyPrimitiveAtom);
+    const todoLists = get(TodoListsAtom);
+    if (currentTodoListKey == "") {
+      if (Object.keys(todoLists).length == 0) {
+        return "";
+      }
+      const firstTodoListKey = Object.keys(todoLists)[0];
+      return firstTodoListKey;
+    } else {
+      if (currentTodoListKey in todoLists) return currentTodoListKey;
+      const firstTodoListKey = Object.keys(todoLists)[0];
+      return firstTodoListKey;
+    }
+  },
+  (get, set, update: string) => set(currentTodoListKeyPrimitiveAtom, update)
+);
