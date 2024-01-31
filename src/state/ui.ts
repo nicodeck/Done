54f35b5ba136daabd684550a892de01d81a320e4
Nicolas Descamps <nicolas.descamps@student-cs.fr>
2024-01-31 13:07:13 +0100

    Remove atoms associated with the modal

--- src/state/ui.ts
+++ src/state/ui.ts
@@ -21,7 +21,3 @@ export const currentTodoListKeyAtom = atom(
   },
   (get, set, update: string) => set(currentTodoListKeyPrimitiveAtom, update)
 );
-
-export const taskModalIsVisibleAtom = atom(false);
-
-export const taskModalTaskKeyAtom = atom("");
